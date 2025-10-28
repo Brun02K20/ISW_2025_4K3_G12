e87513e7f0fe57dc126134c8cd82745d4a2e2a23
EmmaCh <emmanuelricardochaile@hotmail.com>
2025-10-28 12:59:32 -0300

    fix: se reemplazó la paleta de colores para jardinería

--- Entregas/Trabajos_Practicos/TP06_Test Driven Development (TDD)/front/data/activitiesStyles.ts
+++ Entregas/Trabajos_Practicos/TP06_Test Driven Development (TDD)/front/data/activitiesStyles.ts
@@ -1,6 +1,6 @@
 export const activityStyles: Record<string, { icon: string; color: string }> = {
   Palestra: { icon: '🧗‍♂️', color: '#134611' },
   Safari: { icon: '🦁', color: '#3DA35D' },
-  Jardineria: { icon: '🌱', color: '#E8FCCF' },
+  Jardineria: { icon: '🌱', color: '#97d848ff' },
   Tirolesa: { icon: '🪂', color: '#96E072' },
 };
